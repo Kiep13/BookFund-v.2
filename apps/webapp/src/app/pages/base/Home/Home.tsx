949946b8762953b5e0812eeecc7104725e8a120c
Kiep13 <kiep13@bk.ru>
2022-03-23 20:56:58 +0300

    BOOK-35 - Base logic of home page

--- apps/webapp/src/app/pages/base/Home/Home.tsx
+++ apps/webapp/src/app/pages/base/Home/Home.tsx
@@ -1,7 +1,115 @@
-import { wrapUserPage } from '@components/PageWrapper';
-import { compose } from '@utils/helpers';
+import { Box, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-const Page = () => <span>Home page works</span>
+import { wrapUserPage } from '@components/PageWrapper';
+import { BookPromoCard } from '@components/cards/BookPromoCard';
+import { CollectionCard } from '@components/cards/ColllectionCard';
+import { StatefulCard } from '@components/cards/StatefulCard';
+import { getIsAuthorized, getUser } from '@store/reducers';
+import { API_TOOLTIP_ERROR } from '@utils/constants';
+import { CardStates, SortDirections } from '@utils/enums';
+import { IBook, ICollection, IListApiView, ISearchOptions } from '@utils/interfaces';
+import { compose } from '@utils/helpers';
+import { useAlerts, useApi } from '@utils/hooks';
+
+import { STYLES } from './constants';
+
+const Page = () => {
+  const [books, setBooks] = useState<IBook[]>([]);
+  const [collections, setCollections] = useState<ICollection[]>([]);
+  const [state, setState] = useState<CardStates>(CardStates.LOADING);
+  const { getBooks, getCollections } = useApi();
+  const { addError } = useAlerts();
+
+  const isAuthorized = useSelector(getIsAuthorized);
+  const user = useSelector(getUser);
+
+  const loadBooks = () => {
+    const searchOptions: ISearchOptions =  {
+      pageSize: 10,
+      page: 0,
+      order: SortDirections.Asc,
+      orderBy: 'createdAt'
+    }
+
+    getBooks(searchOptions)
+      .then((response: IListApiView<IBook>) => {
+        setBooks(response.data);
+        loadCollections();
+      })
+      .catch(() => {
+        addError(API_TOOLTIP_ERROR);
+        setState(CardStates.ERROR);
+      })
+  };
+
+  const loadCollections = () => {
+    const searchOptions: ISearchOptions =  {
+      pageSize: 10,
+      page: 0,
+      order: SortDirections.Asc,
+      orderBy: 'createdAt'
+    }
+
+    getCollections(searchOptions)
+      .then((response: IListApiView<ICollection>) => {
+        setCollections(response.data);
+        setState(CardStates.CONTENT);
+      })
+      .catch(() => {
+        addError(API_TOOLTIP_ERROR);
+        setState(CardStates.ERROR);
+      })
+  }
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  return (
+    <Box sx={STYLES.page}>
+      {
+        isAuthorized &&
+        <Typography variant='h3' gutterBottom component='div' sx={STYLES.greeting}>
+          Welcome back, {user?.name}
+        </Typography>
+      }
+
+      <StatefulCard state={state}>
+        <Typography variant='h4' gutterBottom component='div' sx={STYLES.booksHeading}>
+          Top 10 new books
+        </Typography>
+
+        <Box sx={STYLES.booksWrapper}>
+          {
+            books.map((book: IBook) =>
+              <Box key={book.id} sx={STYLES.book}>
+                <BookPromoCard book={book}/>
+              </Box>
+            )
+          }
+        </Box>
+
+        <Typography variant='h4' gutterBottom component='div' sx={STYLES.booksHeading}>
+          Collections
+        </Typography>
+
+        <Box sx={STYLES.collectionsWrapper}>
+          {
+            collections.map((collection: ICollection) =>
+              <Box key={collection.id}>
+                <CollectionCard
+                  collection={collection}
+                  isActionsAvailable={true}/>
+              </Box>
+            )
+          }
+        </Box>
+      </StatefulCard>
+    </Box>
+  )
+}
 
 export const Home = compose(
   wrapUserPage()
---
+++ apps/webapp/src/app/pages/base/Home/constants/styles.ts
@@ -0,0 +1,27 @@
+export const STYLES = {
+  page: {
+    maxWidth: 900,
+    m: 'auto'
+  },
+  greeting: {
+    fontWeight: 100
+  },
+  booksHeading: {
+    fontWeight: 100
+  },
+  booksWrapper: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+    gap: '10px',
+    mb: 4
+  },
+  book: {
+    display: 'inline-grid',
+    width: 'fit-content'
+  },
+  collectionsWrapper: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '10px',
+  }
+}
