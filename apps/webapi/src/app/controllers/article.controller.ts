b976bfe6637a21a5f3f825746cc29442c8f0c064
Kiep13 <kiep13@bk.ru>
2022-05-17 14:19:31 +0300

    BOOK-13 - Base logic for parsing content

--- apps/webapi/src/app/controllers/article.controller.ts
+++ apps/webapi/src/app/controllers/article.controller.ts
@@ -1,13 +1,56 @@
 import { Request, Response } from 'express';
+import * as jsdom from 'jsdom';
 
-import { ResponseStatuses } from '@core/enums';
+const {JSDOM} = jsdom;
+
+const POSSIBLE_ROOT_CLASS_NAMES = [
+  'content',
+  'content-inner',
+  'container',
+  'container-block',
+  'main'
+]
 
 class ArticleController {
   public async createArticle(request: Request, response: Response, next: Function): Response {
     try {
       const url = request.body.url;
 
-      return response.status(ResponseStatuses.STATUS_CREATED).json(request.body);
+      const dom = await JSDOM.fromURL(url, {
+        includeNodeLocations: true,
+        storageQuota: 10000000
+      });
+      const document = dom.window.document;
+
+      let content = '';
+
+      for (let i = 0; i < POSSIBLE_ROOT_CLASS_NAMES.length; i++) {
+        let result = document.getElementsByClassName(`${POSSIBLE_ROOT_CLASS_NAMES[i]}`);
+
+        if(result.length && result[0]) {
+          content = result[0].innerHTML;
+          break;
+        }
+
+        result = document.getElementById(`${POSSIBLE_ROOT_CLASS_NAMES[i]}`);
+
+        if(result) {
+          content = result.innerHTML;
+          break;
+        }
+      }
+
+      if(!content) {
+        throw new Error('Error due attempt to find page content');
+      }
+
+      const result = {
+        by: document.location.origin,
+        title: document.title,
+        html: content
+      }
+
+      response.json(result);
     } catch (error) {
       next(error)
     }
