4808de23ec251e2108d8dd3f14c65625fd781c5d
Kiep13 <kiep13@bk.ru>
2022-07-11 15:53:29 +0300

    BOOK-168 - Refactor routes

--- apps/webapi/src/app/create-api-server.ts
+++ apps/webapi/src/app/create-api-server.ts
@@ -3,23 +3,9 @@ import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser';
 import * as fileUpload from 'express-fileupload';
 
-import { ApiRoutes } from '@core/enums';
 import { environment } from '@environments/environment';
 import { errorMiddleware } from '@middlewares/error-middleware';
-import articleRoutes from '@routes/article.routes';
-import authRoutes from '@routes/auth.routes';
-import authorRoutes from '@routes/author.routes';
-import bookRoutes from '@routes/book.routes';
-import genreRoutes from '@routes/genre.routes';
-import favoriteRoutes from '@routes/favorite.routes';
-import folderRoutes from '@routes/folder.routes';
-import collectionRoutes from '@routes/collection.routes';
-import commentRoutes from '@routes/comment.routes';
-import imageRoutes from '@routes/image.routes';
-import readRoutes from '@routes/read.routes';
-import searchRoutes from '@routes/search.routes';
-import statisticsRouter from '@routes/statistics.router';
-import userRoutes from '@routes/user.router';
+import routes from '@routes/index';
 
 const apiServer = express();
 
@@ -37,20 +23,7 @@ apiServer.use(express.static(`../${environment.imagesFolder}`));
 apiServer.use(express.static(`../${environment.booksFolder}`));
 apiServer.use(express.static(`../${environment.articlesFolder}`));
 
-apiServer.use(`/v1/${ApiRoutes.ARTICLE}`, articleRoutes);
-apiServer.use(`/v1/${ApiRoutes.AUTH}`, authRoutes);
-apiServer.use(`/v1/${ApiRoutes.AUTHOR}`, authorRoutes);
-apiServer.use(`/v1/${ApiRoutes.BOOK}`, bookRoutes);
-apiServer.use(`/v1/${ApiRoutes.GENRE}`, genreRoutes);
-apiServer.use(`/v1/${ApiRoutes.FAVORITE}`, favoriteRoutes);
-apiServer.use(`/v1/${ApiRoutes.FOLDER}`, folderRoutes);
-apiServer.use(`/v1/${ApiRoutes.COLLECTION}`, collectionRoutes);
-apiServer.use(`/v1/${ApiRoutes.COMMENT}`, commentRoutes);
-apiServer.use(`/v1/${ApiRoutes.IMAGE}`, imageRoutes);
-apiServer.use(`/v1/${ApiRoutes.READ}`, readRoutes);
-apiServer.use(`/v1/${ApiRoutes.SEARCH}`, searchRoutes);
-apiServer.use(`/v1/${ApiRoutes.STATISTIC}`, statisticsRouter);
-apiServer.use(`/v1/${ApiRoutes.USER}`, userRoutes);
+apiServer.use(routes);
 
 apiServer.use(errorMiddleware);
 
--- apps/webapi/src/app/routes/index.ts
+++ apps/webapi/src/app/routes/index.ts
@@ -1,13 +1,36 @@
-export * from './article.routes';
-export * from './auth.routes';
-export * from './author.routes';
-export * from './book.routes';
-export * from './collection.routes';
-export * from './comment.routes';
-export * from './favorite.routes';
-export * from './folder.routes';
-export * from './genre.routes';
-export * from './image.routes';
-export * from './read.routes';
-export * from './search.routes';
-export * from './statistics.router';
+import { Router } from 'express';
+
+import { ApiRoutes } from '@core/enums';
+import articleRoutes from './article.routes';
+import authRoutes from './auth.routes';
+import authorRoutes from './author.routes';
+import bookRoutes from './book.routes';
+import genreRoutes from './genre.routes';
+import favoriteRoutes from './favorite.routes';
+import folderRoutes from './folder.routes';
+import collectionRoutes from './collection.routes';
+import commentRoutes from './comment.routes';
+import imageRoutes from './image.routes';
+import readRoutes from './read.routes';
+import searchRoutes from './search.routes';
+import statisticsRouter from './statistics.router';
+import userRoutes from './user.router';
+
+const router = new Router();
+
+router.use(`/v1/${ApiRoutes.ARTICLE}`, articleRoutes);
+router.use(`/v1/${ApiRoutes.AUTH}`, authRoutes);
+router.use(`/v1/${ApiRoutes.AUTHOR}`, authorRoutes);
+router.use(`/v1/${ApiRoutes.BOOK}`, bookRoutes);
+router.use(`/v1/${ApiRoutes.GENRE}`, genreRoutes);
+router.use(`/v1/${ApiRoutes.FAVORITE}`, favoriteRoutes);
+router.use(`/v1/${ApiRoutes.FOLDER}`, folderRoutes);
+router.use(`/v1/${ApiRoutes.COLLECTION}`, collectionRoutes);
+router.use(`/v1/${ApiRoutes.COMMENT}`, commentRoutes);
+router.use(`/v1/${ApiRoutes.IMAGE}`, imageRoutes);
+router.use(`/v1/${ApiRoutes.READ}`, readRoutes);
+router.use(`/v1/${ApiRoutes.SEARCH}`, searchRoutes);
+router.use(`/v1/${ApiRoutes.STATISTIC}`, statisticsRouter);
+router.use(`/v1/${ApiRoutes.USER}`, userRoutes);
+
+export default router;
