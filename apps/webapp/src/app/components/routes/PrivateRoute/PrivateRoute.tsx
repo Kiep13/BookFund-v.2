08fdab42d7eb585919519e6506a7d0be8cf031ba
Kiep13 <kiep13@bk.ru>
2023-06-12 17:52:01 +0200

    BOOK-184 - Refactor private route

--- apps/webapp/src/app/components/routes/PrivateRoute/PrivateRoute.tsx
+++ apps/webapp/src/app/components/routes/PrivateRoute/PrivateRoute.tsx
@@ -1,11 +1,11 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import {Navigate, Outlet, useLocation} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
-import { Layout } from '@components/Layout';
-import { getIsAdmin, getIsAuthorized } from '@store/reducers';
-import { RELOAD_PATHNAME_STORAGE_KEY } from '@utils/constants';
-import { AuthRoutePaths } from '@utils/enums';
-import { useAuthHandlers, useStorage } from '@utils/hooks';
+import {Layout} from '@components/Layout';
+import {getIsAdmin, getIsAuthorized} from '@store/reducers';
+import {RELOAD_PATHNAME_STORAGE_KEY} from '@utils/constants';
+import {AuthRoutePaths} from '@utils/enums';
+import {useAuthHandlers, useStorage} from '@utils/hooks';
 
 export const PrivateRoute = () => {
   const location = useLocation();
@@ -15,20 +15,16 @@ export const PrivateRoute = () => {
   const {saveToStorage} = useStorage();
   const {handleLogOut} = useAuthHandlers();
 
-  const render = () => {
-    if (!isAuthorized) {
-      saveToStorage(RELOAD_PATHNAME_STORAGE_KEY, location.pathname);
-      return <Navigate to={`${AuthRoutePaths.REFRESH}`} replace/>;
-    }
-
-    if (!isAdmin) {
-      saveToStorage(RELOAD_PATHNAME_STORAGE_KEY, location.pathname);
-      handleLogOut(`You don't have access to this page`);
-      return <Navigate to={`${AuthRoutePaths.LOGIN}`} replace/>;
-    }
-
-    return <Layout><Outlet/></Layout>;
+  if (!isAuthorized) {
+    saveToStorage(RELOAD_PATHNAME_STORAGE_KEY, location.pathname);
+    return <Navigate to={`${AuthRoutePaths.REFRESH}`} replace/>;
   }
 
-  return render();
+  if (!isAdmin) {
+    saveToStorage(RELOAD_PATHNAME_STORAGE_KEY, location.pathname);
+    handleLogOut(`You don't have access to this page`);
+    return <Navigate to={`${AuthRoutePaths.LOGIN}`} replace/>;
+  }
+
+  return <Layout><Outlet/></Layout>;
 }
